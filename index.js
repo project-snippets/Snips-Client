dcaeddb58f5b20f4036385ec2b1a5e3ed46e164e
Andrew P Maney <amaney@usc.edu>
2019-08-15 18:16:43 -0700

    Started search functionality

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
 import SNIPS from './snippet-data.js';
-
-const renderSnips = snippets => {
-  // map over the SNIPS
-  //    transform that snip into the HTML
-  const snippetHTML = snippets
-    .map(
-      snippet => /* html */ `
-  <div class="snip">
-    <div class="text">
-      <h2>${snippet.title}</h2>
-      <p>${snippet.description}</p>
-    </div>
-    <pre><code class="${snippet.language}">${he.encode(
-        snippet.code
-      )}</code></pre>
-  </div>`
-    )
-    .join('');
-
-  // put that HTML right into #snippets
-  const snippetsElem = document.getElementById('snippets');
-  snippetsElem.innerHTML = snippetHTML;
-};
+import { renderSnips } from './lib/snippets.js';
+import search from './lib/search.js';
 
 renderSnips(SNIPS);
-
 hljs.initHighlightingOnLoad();
