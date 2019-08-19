dc9be48410fedc12097bcb56e64ea94e80ba7981
Jonathan Azali <azali.jonathan@gmail.com>
2019-08-19 11:10:59 -0700

    udpates on searchbar

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ import search from './lib/search.js';
 
 renderSnips(SNIPS);
 
-search('Hello');
+const searchForm = document.getElementById('search-bar');
+const searchText = document.getElementById('search-text');
+
+searchForm.addEventListener('submit', event => {
+  event.preventDefault();
+  search(searchText.value);
+});
 
 hljs.initHighlightingOnLoad();
